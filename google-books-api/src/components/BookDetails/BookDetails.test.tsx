9affb9c0bac819798ecc52eeb4b978378b53dfaa
ashleykouch <ashleykouch99@gmail.com>
2023-04-19 01:28:29 +1000

    added test cases for book details - to fix current error handling

--- google-books-api/src/components/BookDetails/BookDetails.test.tsx
+++ google-books-api/src/components/BookDetails/BookDetails.test.tsx
@@ -44,4 +44,45 @@ describe("BookDetails", () => {
       "https://path/to/mock-book-thumbnail.jpg"
     );
   });
+
+  //   TODO: fix test case for below
+
+  //   const axiosSpy = jest
+  //     .spyOn(axios, "get")
+  //     .mockRejectedValue(new Error("Request failed"));
+
+  test("displays error message when API call is unsuccessful", async () => {
+    // axios.get = jest.fn().mockRejectedValue(new Error("Request failed"));
+
+    const error = new Error("Request failed");
+    const axiosSpy = jest.spyOn(axios, "get").mockRejectedValue(error);
+
+    expect(axiosSpy).toHaveBeenCalledTimes(0);
+
+    render(
+      <MemoryRouter initialEntries={["/book/HI12BYE"]}>
+        <Route path="/book/:id">
+          <BookDetails />
+        </Route>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText(/Loading/i)).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(axiosSpy).toHaveBeenCalledTimes(1);
+    });
+
+    expect(screen.queryByText(/Loading/i)).not.toBeInTheDocument();
+    // expect(await screen.findByText("Mock Book Tester")).toBeInTheDocument();
+    // expect(screen.getByText("Author1, Author2, Author3")).toBeInTheDocument();
+    // expect(
+    //   screen.getByText("Description test for mock book.")
+    // ).toBeInTheDocument();
+    // expect(screen.getByText("Published Date: 2009-10-23")).toBeInTheDocument();
+    // expect(screen.getByText("Page Count: 100")).toBeInTheDocument();
+    expect(screen.getByText(/Request failed/i)).toBeInTheDocument();
+
+    axiosSpy.mockRestore();
+  });
 });
