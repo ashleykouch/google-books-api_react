ab3080de3a5383337ef1481418ecdd9ae725291e
ashleykouch <ashleykouch99@gmail.com>
2023-04-18 17:04:25 +1000

    added footer and styling for contact page

--- google-books-api/src/components/Navbar/Contact/Contact.tsx
+++ google-books-api/src/components/Navbar/Contact/Contact.tsx
@@ -1,46 +1,52 @@
 import React from "react";
 import "./Contact.scss";
+import { Link } from "react-router-dom";
 
 const Contact: React.FC = () => {
   return (
     <div className="contact-page">
-      <h2>Contact Us</h2>
-      <p>
-        If you have any questions, please feel free to reach out through the
-        contact form below:
-      </p>
-      <form className="contact-form">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Your name"
-          required
-        />
+      <div className="contact-container">
+        <Link to="/" className="back_link">
+          Back
+        </Link>
+        <h2>Contact Us</h2>
+        <p>
+          If you have any questions, please feel free to reach out through the
+          contact form below:
+        </p>
+        <form className="contact-form">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Your name"
+            required
+          />
 
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Your email"
-          required
-        />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your email"
+            required
+          />
 
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          name="message"
-          rows={6}
-          placeholder="Your message"
-          required
-        ></textarea>
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            rows={6}
+            placeholder="Your message"
+            required
+          ></textarea>
 
-        <button type="submit" className="submit-btn">
-          Send Message
-        </button>
-      </form>
+          <button type="submit" className="submit-btn">
+            Send Message
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
